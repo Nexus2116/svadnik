adaba6202388b5f3ed9fa3a64244b6788c38b422
Alexey <nexus2116@inbox.ru>
2016-03-15 17:12:49 +0300

    index controller return array

--- app/controller/index.php
+++ app/controller/index.php
@@ -1,102 +1,112 @@
 <?php
 
 namespace Controller;
-	
-	class Index extends \Core\Controller {
 
-		public function __construct() {
-			parent::__construct();
-		}
+class Index extends \Core\Controller
+{
 
-		public function index() {
-			$this->view->layout = 'index';
-			$this->seo('Главная страница', 'Цитадель', 'Цитадель');
-			\App::view('indexPage', true);
-			// \App::view('hideServices', true);
+   public function __construct()
+   {
+      parent::__construct();
+   }
 
-			$catalog = \Model\Articles::where('url', 'services-catalog')->first();
+   public function index()
+   {
+      $this->view->layout = 'index';
+      $this->seo('Главная страница', 'Цитадель', 'Цитадель');
+      \App::view('indexPage', true);
+      // \App::view('hideServices', true);
 
-			$catalog_parent = \Model\Articles::where('parent_id', $catalog->id)->get();
-			\App::view('catalog', $catalog_parent);
+      $catalog = \Model\Articles::where('url', 'services-catalog')->first();
 
-			foreach($catalog_parent as $item)
-				$serviceUserCount[$item['id']] = \Model\Service::where('tagid', $item['id'])->where('deleted', null)->count();
+      $catalog_parent = \Model\Articles::where('parent_id', $catalog->id)->get();
+      \App::view('catalog', $catalog_parent);
 
-			\App::view('serviceUserCount', $serviceUserCount);
+      foreach($catalog_parent as $item)
+         $serviceUserCount[$item['id']] = \Model\Service::where('tagid', $item['id'])->where('deleted', null)->count();
 
-			
-			$catalog_parent = \Model\Articles::where('parent_id', $catalog->id)->take(2)->get();
-			\App::view('catalog_index', $catalog_parent);
-
-
-			//Примерный бюджет вашего мероприятия
-			$offers = \Model\Articles::where('url', 'special-offers')->first();
-			$offersGet = \Model\Articles::getPage('parent_id', $offers->id)->get();
-			\App::view('offers', $offersGet);
+      \App::view('serviceUserCount', $serviceUserCount);
 
 
-			$projects = \Model\Projects::get();
-			\App::view('projects', $projects);
-
-		}
-
-		public function projectAdd_post(){
-
-			$project = new \Model\Projects();
-			$project->title = $_POST['req-title'];
-			$project->date_start = $_POST['req-date-since'];
-			$project->date_end = $_POST['req-date-to'];
-			$project->text = $_POST['req-text'];
-			$project->phone = $_POST['req-phone'];
-			$project->email = $_POST['req-mail'];
-			$project->budget = $_POST['req-budget'];
-			$project->services = $_POST['service-arr'];
-			$project->visible = $_POST['req-chb'];
-			$project->save();
-
-			exit;
-		}
-
-		public function projectsInfo(){
-			$project = \Model\Projects::where('id', $_GET['id'])->first();
-			$arr = Array(
-				'title'=>$project->title,
-				'content'=>$project->text,
-				'date_add_n'=>$project->created_at,
-				'user_phone'=>$project->phone,
-				'user_email'=>$project->email,
-				'attr_id'=>$project->id,
-			);
-			echo json_encode($arr);
-			exit;
-		}
-
-		public function projectOfferAdd(){
-			$user = \Model\Users::where('id', \App::session('user')->id)->first();
-			$arr = explode(',', $user->projects);
-
-			if(!array_search($_GET['id'], $arr)){
-				array_push($arr, $_GET['id']);
-				$result = implode(',', $arr);
-				\Model\Users::where('id', \App::session('user')->id)->update(array('projects'=> $result));
-				echo 'Проект добавлен';
-			}else{
-				echo 'У вас уже имеется данный проект';
-			}
-
-			exit;
-		}
-
-		public function search(){
-			\App::view('hideServices', true);
-			
-
-		}
+      $catalog_parent = \Model\Articles::where('parent_id', $catalog->id)->take(2)->get();
+      \App::view('catalog_index', $catalog_parent);
 
 
+      //Примерный бюджет вашего мероприятия
+      $offers = \Model\Articles::where('url', 'special-offers')->first();
+      $offersGet = \Model\Articles::getPage('parent_id', $offers->id)->get();
+      \App::view('offers', $offersGet);
 
 
+      $projects = \Model\Projects::get();
+      \App::view('projects', $projects);
 
-	}
+   }
+
+   public function projectAdd_post()
+   {
+
+      foreach($_POST as $key => $item){
+         $_POST[$key] = htmlspecialchars($item);
+      }
+      $project = new \Model\Projects();
+      $project->title = $_POST['req-title'];
+      $project->date_start = $_POST['req-date-since'];
+      $project->date_end = $_POST['req-date-to'];
+      $project->text = $_POST['req-text'];
+      $project->phone = $_POST['req-phone'];
+      $project->email = $_POST['req-mail'];
+      $project->budget = $_POST['req-budget'];
+      $project->services = $_POST['service-arr'];
+      $project->visible = $_POST['req-chb'];
+      if($project->save()){
+         echo json_encode(['status' => 1]);
+         exit;
+      }
+      echo json_encode(['status' => 0]);
+      exit;
+   }
+
+   public function projectsInfo()
+   {
+      $project = \Model\Projects::where('id', $_GET['id'])->first();
+      $arr = Array(
+            'title' => $project->title,
+            'content' => $project->text,
+            'date_add_n' => $project->created_at,
+            'user_phone' => $project->phone,
+            'user_email' => $project->email,
+            'attr_id' => $project->id,
+      );
+      echo json_encode($arr);
+      exit;
+   }
+
+   public function projectOfferAdd()
+   {
+      $user = \Model\Users::where('id', \App::session('user')->id)->first();
+      $arr = explode(',', $user->projects);
+
+      if(!array_search($_GET['id'], $arr)){
+         array_push($arr, $_GET['id']);
+         $result = implode(',', $arr);
+         \Model\Users::where('id', \App::session('user')->id)->update(array('projects' => $result));
+         echo 'Проект добавлен';
+      } else{
+         echo 'У вас уже имеется данный проект';
+      }
+
+      exit;
+   }
+
+   public function search()
+   {
+      \App::view('hideServices', true);
+
+
+   }
+
+
+}
 
 ?>
--- app/controller/signup.php
+++ app/controller/signup.php
@@ -1,110 +1,115 @@
 <?php
 
 namespace Controller;
-	
-	class Signup extends \Core\Controller {
 
-		public function __construct() {
-			parent::__construct();
-		}
+class Signup extends \Core\Controller
+{
 
-		public function reg(){
-			
-			echo true;
-			exit;
-		}
+   public function __construct()
+   {
+      parent::__construct();
+   }
 
+   public function reg()
+   {
 
-		public function reg_post(){
-			$valid = Array(
-				'firstname'=>'Имя*',
-				'city'=>'Выберите город*:',
-				'email'=>'E-mail:',
-				'info'=>'О вас:',
-				'lastname'=>'Фамилия',
-				'password'=>'Пароль*:',
-				'passconf'=>'Повторите пароль*:'
-			);
-
-			foreach($valid as $key => $item){
-				if($_POST[$key] == $item)
-					$_POST[$key] = null;
-			}
-
-			$user = new \Model\Users;
-			$user->email = $_POST['email'];
-			$user->password = md5($_POST['password']);
-			$user->role = $_POST['role'];
-			$user->firstname = $_POST['firstname'];
-			$user->city = $_POST['city'];
-			$user->lastname = $_POST['lastname'];
-			$user->newsletter = $_POST['newsletter'];
-			$user->info = $_POST['info'];
-
-			if(!empty($_FILES['userfiles']['size'])){
-				$upload = new \Service\Upload;
-				$upload->uploadImage();
-				$scales = \App::config('scales');
-				$imageService = new \Service\Image;
-				$imageService->resize($scales['avatar'], $upload->files);
-				$user->avatar = $upload->files[0]['newname'];
-			}
-			$user->save();
+      echo json_encode(['stats' => 1]);
+      exit;
+   }
 
 
+   public function reg_post()
+   {
+      $valid = Array(
+            'firstname' => 'Имя*',
+            'city' => 'Выберите город*:',
+            'email' => 'E-mail:',
+            'info' => 'О вас:',
+            'lastname' => 'Фамилия',
+            'password' => 'Пароль*:',
+            'passconf' => 'Повторите пароль*:'
+      );
 
-			$auth = \Model\Users::where('email', $_POST['email'])->first();
-			$obj = (object) array();
-			$obj->id = $auth->id;
-			$obj->role = $auth->role;
-			$obj->firstname = $auth->firstname;
-			\App::session('user', $obj);
-			
-			\Core\Response::navigate('/edit');
-		}
+      foreach($valid as $key => $item){
+         if($_POST[$key] == $item)
+            $_POST[$key] = null;
+      }
 
-		public function login_post(){
+      $user = new \Model\Users;
+      $user->email = $_POST['email'];
+      $user->password = md5($_POST['password']);
+      $user->role = $_POST['role'];
+      $user->firstname = $_POST['firstname'];
+      $user->city = $_POST['city'];
+      $user->lastname = $_POST['lastname'];
+      $user->newsletter = $_POST['newsletter'];
+      $user->info = $_POST['info'];
 
-			if(preg_match('/@/', $_POST['login'])){
-				$login = 'email';
-			}else{
-				$login = 'login';
-			}
-			$this->sess(\Model\Users::where($login, $_POST['login'])->where('password', md5($_POST['password']))->first());
-			exit;
-		}
-
-		protected function sess($login){
-			if(!empty($login)){
-				$obj = (object) array();
-				$obj->id = $login->id;
-				$obj->role = $login->role;
-				$obj->firstname = $login->firstname;
-				\App::session('user', $obj);
-				echo true;
-			}	
-			else
-				echo 0;
-		}
-
-		public function logout(){
-			\App::session('user', null);
-			\Core\Response::navigate('/');
-		}
-
-		public function ckeckEmail(){
-			if(isset($_GET['regmail'])){
-				$check = \Model\Users::where('email', $_GET['regmail'])->first();
-				if(!isset($check->id))
-					echo json_encode(true);
-			}
-			exit;
-		}
+      if(!empty($_FILES['userfiles']['size'])){
+         $upload = new \Service\Upload;
+         $upload->uploadImage();
+         $scales = \App::config('scales');
+         $imageService = new \Service\Image;
+         $imageService->resize($scales['avatar'], $upload->files);
+         $user->avatar = $upload->files[0]['newname'];
+      }
+      $user->save();
 
 
+      $auth = \Model\Users::where('email', $_POST['email'])->first();
+      $obj = (object)array();
+      $obj->id = $auth->id;
+      $obj->role = $auth->role;
+      $obj->firstname = $auth->firstname;
+      \App::session('user', $obj);
+
+      \Core\Response::navigate('/edit');
+   }
+
+   public function login_post()
+   {
+
+      if(preg_match('/@/', $_POST['login'])){
+         $login = 'email';
+      } else{
+         $login = 'login';
+      }
+      $this->sess(\Model\Users::where($login, $_POST['login'])->where('password', md5($_POST['password']))->first());
+      exit;
+   }
+
+   protected function sess($login)
+   {
+      if(!empty($login)){
+         $obj = (object)array();
+         $obj->id = $login->id;
+         $obj->role = $login->role;
+         $obj->firstname = $login->firstname;
+         \App::session('user', $obj);
+         echo json_encode(['stats' => 1]);
+      } else
+         echo json_encode(['stats' => 0]);
+   }
+
+   public function logout()
+   {
+      \App::session('user', null);
+      \Core\Response::navigate('/');
+   }
+
+   public function ckeckEmail()
+   {
+      if(isset($_GET['regmail'])){
+         $check = \Model\Users::where('email', $_GET['regmail'])->first();
+         if(!isset($check->id))
+            echo json_encode(['stats' => 1]);
+         exit;
+      }
+      echo json_encode(['stats' => 0]);
+      exit;
+   }
 
 
-
-	}
+}
 
 ?>
